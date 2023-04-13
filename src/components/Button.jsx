7149e21612c9d5115adf6edaa1a32e483c30ffce
Atabegovv <atabegov98@mail.ru>
2023-04-14 00:20:41 +0300

    added comp

---
+++ src/components/Button.jsx
@@ -0,0 +1,7 @@
+function Button(props) {
+	const { onClick } = props;
+
+	return <button onClick={onClick}>click me!</button>;
+}
+
+export default Button;
---
+++ src/components/Counter.jsx
@@ -0,0 +1,7 @@
+function Counter(props) {
+	const { count } = props;
+
+	return <h1>Total click: {count}</h1>;
+}
+
+export default Counter;
